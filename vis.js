62e2a66e396464dd4b19152b9f745d124a123971
Eamox <eaamox@gmail.com>
2021-06-18 18:27:40 +1000

    Update vis.js

--- vis.js
+++ vis.js
@@ -1,25 +1,31 @@
 looker.plugins.visualizations.add({
-    create: function(element, config) {
+  create: function(element, config) {
 
   },
-    updateAsync: function(data, element, config, queryResponse, details, done) {
-  post=data[0][queryResponse.fields.dimensions[0].name].value
+  updateAsync: function(data, element, config, queryResponse, details, done) {
 
+    post = data[0][queryResponse.fields.dimensions[0].name].value;
 
-var body = document.getElementsByTagName('body')[0];
-var di = document.createElement('div');
-di.innerHTML=`
-<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="${post}?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="13" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:6px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
+    var block;
+    var post_full = `${post}?utm_source=ig_embed&utm_campaign=loading`
+    if (block = document.getElementsByTagName('blockquote')[0]) {
+      var current = block.getAttribute('data-instgrm-permalink')
+      if (current == post_full) {
+        return true
+      }
+    };
+    var body = document.getElementsByTagName('body')[0];
+    body.innerHTML = "";
+    var di = document.createElement('div');
+    di.innerHTML = `
+<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="${post}?utm_source=ig_embed&utm_campaign=loading" data-instgrm-version="13" style=" background:#FFF; border:0;height: 1000px; border-radius:3px; margin: 1px; max-width:540px; min-width:6px; padding:0;width:99%" scrolling=true>
 
 </blockquote>
-`
-body.appendChild(di)
-var tag = document.createElement('script');
-tag.async = true;
-tag.src = '//www.instagram.com/embed.js';
-var body = document.getElementsByTagName('body')[0];
-body.appendChild(tag);
-
+`;
+    body.appendChild(di);
+    var tag = document.createElement('script');
+    //tag.async = true;
+    tag.src = 'https://www.instagram.com/embed.js';
+    body.appendChild(tag).remove()
   }
 })
-
