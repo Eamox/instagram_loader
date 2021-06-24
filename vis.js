2a1ef55e00965c9b53885d02edeb6ff5276e8038
Eamonn O'hanlon <eaamox@gmail.com>
2021-06-24 15:19:44 +1000

    cleaner code

--- vis.js
+++ vis.js
@@ -1,31 +1,41 @@
 looker.plugins.visualizations.add({
-  create: function(element, config) {
+    create: function(element, config) {
 
-  },
-  updateAsync: function(data, element, config, queryResponse, details, done) {
+	
+	//embed script
+	var tag = document.createElement('script');
+	tag.async = true;
+	tag.src = 'https://www.instagram.com/embed.js';
+	element.appendChild(tag)
 
-    post = data[0][queryResponse.fields.dimensions[0].name].value;
+	//make container
+	this._container = document.createElement('div').id('insta-container')
+	element.append(this._container)
+	
 
-    var block;
-    var post_full = `${post}?utm_source=ig_embed&utm_campaign=loading`
-    if (block = document.getElementsByTagName('blockquote')[0]) {
-      var current = block.getAttribute('data-instgrm-permalink')
-      if (current == post_full) {
-        return true
-      }
-    };
-    var body = document.getElementsByTagName('body')[0];
-    body.innerHTML = "";
-    var di = document.createElement('div');
-    di.innerHTML = `
-<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="${post}?utm_source=ig_embed&utm_campaign=loading" data-instgrm-version="13" style=" background:#FFF; border:0;height: 1000px; border-radius:3px; margin: 1px; max-width:540px; min-width:6px; padding:0;width:99%" scrolling=true>
+	
 
+    },
+    updateAsync: function(data, element, config, queryResponse, details, done) {
+
+	window.__igEmbedLoaded = window.__igEmbedLoaded || function( loadedItem ) {
+	    this._item = document.getElementById(loadedItem).getAttribute('data-instgrm-permalink')
+	    done()
+	};
+
+	post = `${data[0][queryResponse.fields.dimensions[0].name].value}?utm_source=ig_embed&utm_campaign=loading`
+
+	var block;
+	if (this._item && this._item == post) {
+		done()
+		return true
+	    }
+	};
+	this._container.innerHTML`
+<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="${post}" data-instgrm-version="13" style=" background:#FFF; border:0;height: 1000px; border-radius:3px; margin: 1px; max-width:540px; min-width:6px; padding:0;width:99%" scrolling=true>
 </blockquote>
 `;
-    body.appendChild(di);
-    var tag = document.createElement('script');
-    //tag.async = true;
-    tag.src = 'https://www.instagram.com/embed.js';
-    body.appendChild(tag).remove()
-  }
+    window.instgrm.Embeds.process()
+
+    }
 })
