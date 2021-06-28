57ebc4eed3748ef78d9bda30e7fd2e22a68a8355
Eamox <eaamox@gmail.com>
2021-06-28 14:31:59 +1000

    Update vis.js

--- vis.js
+++ vis.js
@@ -1,51 +1,57 @@
-
 looker.plugins.visualizations.add({
-    create: function(element, config) {
+  create: function(element, config) {
 
-	//embed script
-	var tag = document.createElement('script')
-	tag.src = '//www.instagram.com/embed.js';
-	element.appendChild(tag)
+    //embed script
+    var tag = document.createElement('script')
+    tag.src = '//www.instagram.com/embed.js';
+    element.appendChild(tag)
 
-	//make container
-	this._container = document.createElement('div')
-	this._container.id='insta-container'
-	element.append(this._container)
+    //make container
+    this._container = document.createElement('div')
+    this._container.id = 'insta-container'
+    element.append(this._container)
 
-	//make callback
-	
-
-	
-
-    },
-    updateAsync: function(data, element, config, queryResponse, details, done) {
+    //make callback
 
 
 
-	post = `${data[0][queryResponse.fields.dimensions[0].name].value}?utm_source=ig_embed&utm_campaign=loading`
+
+  },
+  updateAsync: function(data, element, config, queryResponse, details, done) {
 
 
-	if (this._item && this._item == post) {
-	    done()
-	    return true
-	}
 
-	this._item=post
-	
-	this._container.innerHTML=`
+    post = `${data[0][queryResponse.fields.dimensions[0].name].value}?utm_source=ig_embed&utm_campaign=loading`
+
+
+    if (this._item && this._item == post) {
+      done()
+      return true
+    }
+
+    this._item = post
+
+    this._container.innerHTML = `
 <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="${post}" data-instgrm-version="13" style=" background:#FFF; border:0;height: 1000px; border-radius:3px; margin: 1px; max-width:540px; min-width:6px; padding:0;width:99%" scrolling=true>
 </blockquote>
 `;
-	window.instgrm.Embeds.process()
-	var iframe = document.getElementsByTagName("iframe");
-	iframe[0].addEventListener("load", function() {
-	    console.log('done')
-	    done()
-	})
+    window.instgrm.Embeds.process()
+
+    var checkExist = setInterval(function() {
+      if (document.getElementsByTagName("iframe").length) {
+        var iframe = document.getElementsByTagName("iframe");
+        iframe[0].addEventListener("load", function() {
+          console.log('done')
+          done()
+        })
+        clearInterval(checkExist);
+      }
+    }, 100);
 
 
 
 
 
-    }
+
+  }
 })
