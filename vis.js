c1863f5dd93113ef1eb0145089148c9584b37bb5
Eamonn O'hanlon <eaamox@gmail.com>
2021-06-28 15:19:56 +1000

    fixed waiting for script

--- vis.js
+++ vis.js
@@ -1,10 +1,25 @@
+
+function waitForElement(done){
+    if(typeof window.instgrm !== "undefined"){
+	console.log('exists')
+	window.instgrm.Embeds.process()
+	var iframe = document.getElementsByTagName("iframe");
+	iframe[0].addEventListener("load", function() {
+	    console.log('done')
+	    done()
+	})
+    }
+    else{
+        setTimeout(waitForElement, 250);
+    }
+}
+
 looker.plugins.visualizations.add({
   create: function(element, config) {
 
     //embed script
     var tag = document.createElement('script')
     tag.src = '//www.instagram.com/embed.js';
-    tag.async =true
     element.appendChild(tag)
 
     //make container
@@ -26,7 +41,8 @@ looker.plugins.visualizations.add({
 
 
     if (this._item && this._item == post) {
-      console.log("running same")
+	console.log("running same")
+	done()
       return true
     }
 
@@ -36,8 +52,9 @@ looker.plugins.visualizations.add({
 <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="${post}" data-instgrm-version="13" style=" background:#FFF; border:0;height: 1000px; border-radius:3px; margin: 1px; max-width:540px; min-width:6px; padding:0;width:99%" scrolling=true>
 </blockquote>
 `;
-    window.instgrm.Embeds.process()
-    done()
+
+      waitForElement(done)
+      console.log('done function done.')
 
 
 
